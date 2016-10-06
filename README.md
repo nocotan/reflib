43b94e9f6dee6887ec836ba72675e767bac83eff
nocotan <noconoco.lib@gmail.com>
2016-10-06 14:19:36 +0900

    add estd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # reflib
-C++ライブラリ
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)  
+C++ Libraries
+
+###Constitution
+- estd - Effective standard template library
---
+++ reflib/estd.hpp
@@ -0,0 +1,43 @@
+#ifndef REFLIB_ESTD_HPP
+#define REFLIB_ESTD_HPP
+
+#include <algorithm>
+
+namespace reflib {
+    template<typename C, typename T>
+    int count(C& c, T t) {
+        return std::count(c.begin(), c.end(), t);
+    }
+
+    template<typename C, typename F>
+    int count_if(C& c, F f) {
+        return std::count_if(c.begin(), c.end(), f);
+    }
+
+    template<typename C, typename T, typename U>
+    void replace(C& c, T t, U u) {
+        std::replace(c.begin(), c.end(), t, u);
+    }
+
+    template<typename C, typename F, typename U>
+    void replace_if(C& c, F f, U u) {
+        std::replace_if(c.begin(), c.end(), f, u);
+    }
+
+    template<typename C>
+    void sort(C& c) {
+        std::sort(c.begin(), c.end());
+    }
+
+    template<typename C, typename Pred>
+    void sort(C& c, Pred p) {
+        std::sort(c.begin(), c.end(), p);
+    }
+
+    template<typename C>
+    void reverse(C& c) {
+        std::reverse(c.begin(), c.end());
+    }
+}
+
+#endif
